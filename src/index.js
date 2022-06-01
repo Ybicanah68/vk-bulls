3c3992d106c5009aeb84c5be8acbc5fa271cd255
Ybicanah68 <shishcov@bk.ru>
2022-06-01 21:00:16 +0300

    rand funk

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ class CowsAndBulls extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      rand: this.randFunk(),
       attempt: "",
       story: "",
       result: "",
@@ -41,34 +42,48 @@ class CowsAndBulls extends React.Component {
      story: this.state.attempt? this.state.story + this.state.attempt + '\n' : this.state.story + '',
     }));
   }
+  randFunk(){
+    var nums = [1,2,3,4,5,6,7,8,9,10],
+        ranNums = [],
+        i = nums.length,
+        j = 0;
+
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        ranNums.push(nums[j]);
+        nums.splice(j,1);
+    }
+    console.log(nums);
+    return nums;
+  }
   render() {
     return React.createElement(
-    "form",
-    {
-      className: "attemptForm",
-      onSubmit: this.handleSubmit
-    },
-    React.createElement("span", {
-      className: "title",
-    }, "Введите число:"),
-    React.createElement("input", {
-      className: "attempt",
-      type: "text",
-      //placeholder: "Попытка",
-      value: this.state.attempt,
-      onChange: this.handleAttemptChange
-    }),
-    React.createElement("input", {
-      className: "attempt-submit",
-      type: "submit",
-      value: "Подтвердить"
-    }),
-    React.createElement("textarea", {
-      className: "attempt-textarea",
-      readOnly: "readOnly",
-      placeholder: "Ваши попытки",
-      value: this.state.story,
-    })
+      "form",
+      {
+        className: "attemptForm",
+        onSubmit: this.handleSubmit
+      },
+      React.createElement("span", {
+        className: "title",
+      }, "Введите число:"),
+      React.createElement("input", {
+        className: "attempt",
+        type: "text",
+        //placeholder: "Попытка",
+        value: this.state.attempt,
+        onChange: this.handleAttemptChange
+      }),
+      React.createElement("input", {
+        className: "attempt-submit",
+        type: "submit",
+        value: "Подтвердить"
+      }),
+      React.createElement("textarea", {
+        className: "attempt-textarea",
+        readOnly: "readOnly",
+        placeholder: "Ваши попытки",
+        value: this.state.story,
+      })
     );
   }
 }
